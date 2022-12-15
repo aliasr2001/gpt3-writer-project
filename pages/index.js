31547a9f75f3021fd57a7b6c054513824f7be348
AliAsr <aliasr8bp@gmail.com>
2022-12-15 08:54:48 -0700

    Latest Build

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,97 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import { useState } from "react";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAi..");
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAi replied..", output.text);
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  };
+
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Proposalify - Get Cool AI Generated Proposals</title>
       </Head>
+      <div id="navbar">
+        <p className="logotext">Proposalify.</p>
+        <div id="links">
+          <ul>
+            <li><a href="www.google.com" target="_blank">Extension Soon &#128293;</a></li>
+          </ul>
+        </div>
+      </div>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Level Up 🚀 the Freelancing</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Generate Professional & Unique Proposals For Free!</h2>
           </div>
         </div>
+        <div className="prompt-container">
+          <textarea
+            placeholder="Just Paste here the Description of the Project OR the Title of Project - The one who provide more detail about project! Then the Magic Happen :D"
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+        </div>
+        <div className="prompt-buttons">
+          <a
+            className={
+              isGenerating ? "generate-button loading" : "generate-button"
+            }
+            onClick={callGenerateEndpoint}
+          >
+            <div className="generate">
+              {isGenerating ? (
+                <span className="loader"></span>
+              ) : (
+                <p>Generate</p>
+              )}
+            </div>
+          </a>
+        </div>
       </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+      {apiOutput && (
+        <div className="output">
+          <div className="output-header-container">
+            <div className="output-header">
+              <h3>Output</h3>
+            </div>
           </div>
-        </a>
+          <div className="output-content">
+            <p>{apiOutput}</p>
+          </div>
+        </div>
+      )}
+      <div className="footer">
+        <p className="creditbs">Made With Buildspace &#10084;</p>
       </div>
     </div>
   );
